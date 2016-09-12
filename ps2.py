fbc4474ff3f381539e777a86075fb650814fe0af
2016JTM2079 <nitingarg28@live.com>
2016-09-12 16:44:52 +0530

    ps2.py

--- ps2.py
+++ ps2.py
@@ -1,7 +1,9 @@
+#-------Importing Libraries-------
 import random
 import math
 
 count =0
+#User Defined input for no. of points in area
 user_count=input("Enter number of users")
 for points in range(user_count):
 	(x,y)=(random.random()*2- 1, random.random()*2-1)
@@ -12,9 +14,16 @@ for points in range(user_count):
 	var3=math.pow(var3,0.5)
 	if var3<=1:
 		count += 1
+
+
 		continue
+
+#Calculation of points in the given area
 print count
 efficiency=count/float(user_count)*100
+
+
+#Efficiency
 print("The number of user in the circle area :",efficiency)
 
 
