c56fc51a16b176a8fa6a8b90e5043b2a8bf3c171
2016JTM2079 <nitingarg28@live.com>
2016-09-12 16:17:58 +0530

    Ps3.py

--- ps3.py
+++ ps3.py
@@ -1,12 +1,14 @@
+#--------------Database------------
+
 state={'Uttrakhand' : '010','Delhi' : '100','Punjab': '111','Haryana': '101'}
 district={'Roorkee':'101','Rohini ': '100','Bathinda':'001','Hissar':'000'}
 landmark={'IIT ROORKEE': '000','IIT DELHI':'001','IIT ROPAR':'010','IIT ROHTAK':'100'}
 
 print "1.add 2.modify 3.delete 4.query" 
 
-a=input("enter the operation you want to perform")
-
-if a==1: 
+useroption=input("enter the operation you want to perform")
+#------------Add to database--------------------
+if useroption==1: 
 	stt=raw_input("enter the state name you want to add") 
 	code=raw_input("enter the code name you want to add for state")
 	state.update({stt:code}) 
@@ -16,7 +18,9 @@ if a==1:
  	dstrt=raw_input("enter the district name you want to add") 
 	cod=raw_input("enter the code name you want to add for district") 
 	district.update({dstrt:cod})
-if a==2:
+
+#-----------Modify database-----------
+if useroption==2:
  	s1=raw_input("enter the state whose code you want to modify\n") 
 	c1=raw_input("enter the new code\n") 
 	if s1 in state.keys(): 
@@ -41,7 +45,8 @@ else :
 	l3=raw_input("enter the customer landmark\n") 
 	l4=raw_input("enter the customer state\n") 
 	if l1 in state.keys(): 
-		print "CC_NO = " state.get(l4, default=None) 
+		print "CC_NO = " 
+		state.get(l4, default=None) 
 	if l1 in district.keys(): 
 		print district.get(l2, default=None) 
 	if l1 in landmark.keys(): 
@@ -58,8 +63,8 @@ else :
 print state
 
 
-
-if a==3:
+#------------delete database-------------
+if useroption==3:
 	b=raw_input("enter the state want to delete\n") 
 	if b in state.keys(): 
 		del state[b] 
@@ -69,7 +74,7 @@ if a==3:
 	if b in city.keys():
  		del city[b] 
 	else : 
-	print "not a valid key" 
+		print "not a valid key" 
 	b=raw_input("enter the district want to delete\n") 
 	if b in district.keys(): 
 		del district[b] 
